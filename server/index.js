08710d2dc5a4e8671b53683147a739a2dda104d4
Rupesh <rupesh457809@gmail.com>
2026-05-14 19:19:02 +0530

    Fix server exit issue by moving app.listen

--- server/index.js
+++ server/index.js
@@ -34,8 +34,10 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/athiya-dev'
 })
 .then(() => {
   console.log('MongoDB Connected');
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 })
 .catch(err => {
   console.error('Database connection error:', err);
+  process.exit(1);
 });
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
